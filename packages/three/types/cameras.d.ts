2300bf3541e87af05732936c8bd7fa317340ca6d
Leonardo Rick <leonardo.rick@unbabel.com>
2024-01-28 21:09:53 -0300

    fix cameras.d.ts

--- packages/three/types/cameras.d.ts
+++ packages/three/types/cameras.d.ts
@@ -2,4 +2,4 @@ import { Camera } from 'three';
 
 export function saveCameraCoordinates(camera: Camera): void;
 
-export function loadCameraCoordinates(camera: Camera): position;
+export function loadCameraCoordinates(camera: Camera): { position: Vector3; rotation: Vector3 };
